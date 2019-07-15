6fab31dd89da9772baf3e74333fbb897c2b65a3a
Igor <dddartigor@gmail.com>
2019-07-16 01:38:08 +0300

    GUI was added to the client part

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask import jsonify
 from flask import abort
 from flask import request
+from flask import render_template
+from flask_wtf import Form
+from wtforms import TextField, BooleanField
+from wtforms.validators import Required
 
 app = Flask(__name__)
 
@@ -28,29 +32,37 @@ korobchansky = [
 	}
 ]
 
+
+
+@app.route('/index')
+def index():
+    user = { 'nickname': 'Miguel' }
+    return render_template("index.html",
+        title = 'Home',
+        user = user)
+
 @app.route('/api/korobchansky', methods=['GET'])
 def get_korobchansky():
-	
 	return jsonify({'korobchansky': korobchansky})
 
 @app.route('/api/korobchansky/<int:id>', methods=['GET'])
 def get_subject(id):
-    return jsonify({'korobchansky/'+str(id): korobchansky[id-1]})
+    return jsonify(korobchansky[id-1])
 
 @app.route('/api/korobchansky', methods=['POST'])
 def add_subject():
 	subject = {
 		'id': korobchansky[-1]['id'] + 1,
-		'subject': request.args.get('subject'),
-		'rating': request.args.get('rating')
+		'subject': request.json.get('subject', ""),
+		'rating': request.json.get('rating', "")
 	}
 	korobchansky.append(subject)
 	return jsonify({'subject': subject}), 201
 
 @app.route('/api/korobchansky/<int:id>', methods=['PUT'])
 def update_subject(id):
-    korobchansky[id-1]['subject'] = request.args.get('subject')
-    korobchansky[id-1]['rating'] = request.args.get('rating')
+    korobchansky[id-1]['subject'] = request.json.get('subject', "")
+    korobchansky[id-1]['rating'] = request.json.get('rating', "")
     return jsonify({'korobchansky/'+str(id): korobchansky[id-1]})
 
 @app.route('/api/korobchansky/<int:id>', methods=['DELETE'])
